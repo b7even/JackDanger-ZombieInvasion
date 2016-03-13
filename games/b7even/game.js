2b88fafbf3ea6d12f9ae58d2b0c75bb103fc1bec
b7even <joberthel94@gmail.com>
2016-03-13 18:02:40 +0100

    added bombs

--- games/b7even/game.js
+++ games/b7even/game.js
@@ -19,6 +19,8 @@ JackDanger.b7even.prototype.preload = function() {
 	game.load.spritesheet('zombie', 'zombie.png', 32, 64);
 	game.load.image('heart', 'heart.png');
 	game.load.image('dirt', 'dirt.png');
+	game.load.spritesheet('bomb', 'bomb.png', 17, 21);
+	game.load.spritesheet('explosion', 'explosion.png', 38, 38);
 
 	game.load.bitmapFont('carrier_command', 'carrier_command.png', 'carrier_command.xml');
 }
@@ -38,9 +40,15 @@ JackDanger.b7even.prototype.create = function() {
 
             bg_element.x = x + 32;
             bg_element.y = y + 32;
+
             bg_element.angle = Math.round(Math.random() * 3) * 90;
         }
     }
+
+    this.statusMsgShadow = game.add.bitmapText(786 + 3, 424 + 3, 'carrier_command', '', 16);
+    this.statusMsgShadow.anchor.setTo(1, .5);
+    this.statusMsgShadow.tint = 0x000000;
+    this.statusMsgShadow.alpha = .3;
 
     this.statusMsg = game.add.bitmapText(786, 424, 'carrier_command', '', 16);
     this.statusMsg.anchor.setTo(1, .5);
@@ -54,11 +62,34 @@ JackDanger.b7even.prototype.create = function() {
     this.zombies = new this.zombiesDefinition();
     this.zombieT = new Date().getTime();
 
-    this.zombieSpawner = [1000, 5000, 0, 4000, 1000, 3000, 500, 500, 2000, 250, 750, 1000, 0];
+    this.zombieSpawner = [1000, 4000, 4000, 0, 4000, 250, 250, 250, 6000, 2000, 1000, 3000, 3000, 0, 2000, 0, 0, 2000, 2000, 0];
 
     this.zombies.count = this.zombieSpawner.length;
 
     this.statusMsg.setText(this.zombies.count + ' zombies left');
+    this.statusMsgShadow.setText(this.zombies.count + ' zombies left');
+
+    this.bombImage = game.add.sprite(770, 21, 'bomb');
+    this.bombImage.anchor.setTo(0.5, .5);
+    this.bombImage.scale.setTo(2.5, 2.5);
+
+    this.bombMsgShadow = game.add.bitmapText(755 + 3, 30 + 3, 'carrier_command', '', 24);
+    this.bombMsgShadow.anchor.setTo(1, .5);
+    this.bombMsgShadow.tint = 0x000000;
+    this.bombMsgShadow.alpha = .3;
+
+    this.bombMsg = game.add.bitmapText(755, 30, 'carrier_command', '', 24);
+    this.bombMsg.anchor.setTo(1, .5);
+    this.bombMsg.tint = 0xa02d2d;
+
+    this.bombCount = 2;
+
+    this.bombMsg.setText(this.bombCount);
+    this.bombMsgShadow.setText(this.bombCount);
+
+    this.bombs = [];
+
+    this.explosions = [];
 }
 
 //wird jeden Frame aufgerufen
@@ -71,6 +102,47 @@ JackDanger.b7even.prototype.update = function() {
         
         this.zombieT = timestamp;
         this.zombies.add();
+    }
+
+    // bomb explode?
+    for (var i in this.bombs) {
+        if (this.bombs[i].timestamp + 2000 <= timestamp) {
+            var b = this.explosions.length;
+
+            this.explosions[b] = {};
+
+            this.explosions[b].obj = game.add.sprite(this.bombs[i].obj.x, this.bombs[i].obj.y, 'explosion');
+            this.explosions[b].obj.anchor.setTo(.5, .5);
+            this.explosions[b].obj.scale.setTo(6, 6);
+
+            this.explosions[b].obj.animations.add('explode', [0, 1, 2, 3, 4, 5], 6, false);
+
+            this.explosions[b].timestamp = timestamp;
+
+            this.explosions[b].obj.animations.play('explode');
+
+            game.world.remove(this.bombs[i].obj);
+            this.bombs.splice(i, 1);
+
+            // jack to near?
+            var vx = this.explosions[b].obj.x - this.jack.obj.x;
+            var vy = this.explosions[b].obj.y - this.jack.obj.y;
+
+            var distance = Math.sqrt(vx * vx + vy * vy);
+
+            if (distance <= 120) {
+                this.jack.hit();
+            }
+        }
+    }
+
+    for (var i in this.explosions) {
+        if (this.explosions[i].timestamp + 1000 <= timestamp) {
+            game.world.remove(this.explosions[i].obj);
+            this.explosions.splice(i, 1);
+        } else if (this.explosions[i].timestamp + 250 <= timestamp && this.explosions[i].timestamp + 750 >= timestamp) {
+            this.zombies.kill(this.explosions[i].obj.x, this.explosions[i].obj.y, 'Explosion');
+        }
     }
 
     // won?
@@ -100,19 +172,29 @@ JackDanger.b7even.prototype.update = function() {
     }
 
     // arange objects behind each other
-    this.zombies.objects.sort(function (a, b) { return a.y - b.y });
+    var objects = [];
+
+    for (var i in this.zombies.objects) {
+        objects.push(this.zombies.objects[i]);
+    }
+
+    for (var i in this.bombs) {
+        objects.push(this.bombs[i].obj);
+    }
+
+    objects.sort(function (a, b) { return a.y - b.y });
 
     var jack_top = false;
 
-    for (var i in this.zombies.objects) {
-        if (this.zombies.objects[i].y < this.jack.obj.y) {
-            this.zombies.objects[i].bringToTop();
+    for (var i in objects) {
+        if (objects[i].y < this.jack.obj.y) {
+            objects[i].bringToTop();
         } else {
             if (jack_top == false) {
                 this.jack.obj.bringToTop();
                 jack_top = true;
             }
-            this.zombies.objects[i].bringToTop();
+            objects[i].bringToTop();
         }      
     }
 
@@ -124,7 +206,17 @@ JackDanger.b7even.prototype.update = function() {
         this.jack.lifes[i].bringToTop();
     }
 
+    this.explosions.sort(function (a, b) { return a.obj.y - b.obj.y });
+    for (var i in this.explosions) {
+        this.explosions[i].obj.bringToTop();
+    }
+
+    game.world.bringToTop(this.statusMsgShadow);
     game.world.bringToTop(this.statusMsg);
+
+    game.world.bringToTop(this.bombMsgShadow);
+    game.world.bringToTop(this.bombMsg);
+    game.world.bringToTop(this.bombImage);
 }
 
 // ---- //
@@ -144,6 +236,7 @@ JackDanger.b7even.prototype.jackDefinition = function () {
     this.gotHit = false;
 
     this.lastStrike = 0;
+    this.lastBomb = 0;
 
     // create jack
     this.create = function () {
@@ -262,7 +355,20 @@ JackDanger.b7even.prototype.zombiesDefinition = function () {
     };
 
     this.kill = function (x, y, direction) {
-        if (direction == 'Left') {
+        if (direction == 'Explosion') {
+            for (var i in this.objects) {
+                var vx = x - this.objects[i].x;
+                var vy = y - this.objects[i].y;
+
+                var distance = Math.sqrt(vx * vx + vy * vy);
+
+                if (distance <= 120) {
+                    game.world.remove(this.objects[i]);
+                    this.objects.splice(i, 1);
+                    this.count -= 1;
+                }
+            }
+        } else if (direction == 'Left') {
             for (var i in this.objects) {
                 if (Math.abs(this.objects[i].y - y) < 25) {
                     if (x - Math.abs(this.objects[i].x) > 0 && x - Math.abs(this.objects[i].x) < 60) {
@@ -383,10 +489,39 @@ JackDanger.b7even.prototype.controls = function (dt) {
             this.zombies.kill(this.jack.obj.x, this.jack.obj.y, this.jack.direction);
 
             this.statusMsg.setText(this.zombies.count + ' zombies left');
+            this.statusMsgShadow.setText(this.zombies.count + ' zombies left');
 
             this.jack.obj.animations.play('hit' + this.jack.direction);
 
             this.jack.lastStrike = timestamp;
+        }
+    }
+
+    // throw bomb
+    if (Pad.isDown(Pad.JUMP)) {
+        if (this.jack.lastBomb + 350 < timestamp && this.bombCount > 0) {
+            
+            this.bombCount -= 1;
+
+            this.bombMsg.setText(this.bombCount);
+            this.bombMsgShadow.setText(this.bombCount);
+
+            // create bomb
+            var i = this.bombs.length;
+
+            this.bombs[i] = {};
+
+            this.bombs[i].obj = game.add.sprite(this.jack.obj.x, this.jack.obj.y, 'bomb');
+            this.bombs[i].obj.anchor.setTo(.5, 1);
+            this.bombs[i].obj.scale.setTo(2, 2);
+
+            this.bombs[i].obj.animations.add('explode', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], 7, false);
+
+            this.bombs[i].timestamp = timestamp;
+
+            this.bombs[i].obj.animations.play('explode');
+
+            this.jack.lastBomb = timestamp;
         }
     }
 
