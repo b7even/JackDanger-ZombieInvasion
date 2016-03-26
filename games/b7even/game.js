49c88b29fdb1fb267b2ec60b1684391846d322b9
b7even <joberthel94@gmail.com>
2016-03-26 13:58:27 +0100

    framework v4

--- games/b7even/game.js
+++ games/b7even/game.js
@@ -7,7 +7,7 @@ addMyGame("b7even", "Zombie Invasion", "b7even", "Töte die Zombies mit den Tast
 
 JackDanger.b7even.prototype.init = function () {
     logInfo("init Game");
-    addLoadingScreen(this, true);//nicht anfassen
+    addLoadingScreen(this, false);//nicht anfassen
 }
 
 JackDanger.b7even.prototype.preload = function () {
@@ -44,6 +44,10 @@ JackDanger.b7even.prototype.preload = function () {
 JackDanger.b7even.prototype.create = function () {
     Pad.init();//nicht anfassen
     //removeLoadingScreen();//nicht anfassen
+}
+
+JackDanger.b7even.prototype.render = function () {
+    // lol
 }
 
 JackDanger.b7even.prototype.mycreate = function () {
